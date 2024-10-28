0dfab63f4399121122980ce65b8c28aeee40686f
Alex Harvey <alex.harvey@workwave.com>
2024-10-28 11:15:52 +1100

    bun with node-fetch request does not work as intended

---
+++ index.js
@@ -0,0 +1,15 @@
+import { Request } from 'node-fetch';
+
+function main() {
+    const request = new Request('https://jsonplaceholder.typicode.com/posts', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    console.log({ requestUrl: request.url });
+
+    const url = new URL(request.url);
+}
+
+main();
